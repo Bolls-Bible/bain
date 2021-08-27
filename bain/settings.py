7b65ae90a7e4b2391cf9f807cf061d4266bed388
Bohooslav <bpavlisinec@gmail.com>
2021-08-27 20:23:12 +0300

    Fix deploy settings.

--- bain/settings.py
+++ bain/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'bain.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
