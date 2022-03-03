afdca556c2d533915540cc302b30618a3229a8cc
Bohuslav <bpavlisinec@gmail.com>
2022-03-03 12:23:10 +0200

    try container name

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ docker-compose exec web python manage.py migrate --noinput
 docker-compose exec web python manage.py collectstatic --no-input --clear
 
 # Copy & activate unaccent rules
-docker cp ./django/sql/unaccent_plus.rules  46aa08d2b689:/usr/local/share/postgresql/tsearch_data/unaccent_plus.rules
-docker exec -it 46aa08d2b689 psql -U django -d bain -c "ALTER TEXT SEARCH DICTIONARY unaccent (RULES='unaccent_plus')"
+docker cp ./django/sql/unaccent_plus.rules  postgres:/usr/local/share/postgresql/tsearch_data/unaccent_plus.rules
+docker exec -it postgres psql -U django -d bain -c "ALTER TEXT SEARCH DICTIONARY unaccent (RULES='unaccent_plus')"
 
 echo "Done."
