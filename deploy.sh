745060461b853e1bdd74a92fa82a53d0277793cf
ahvth <ah@qodex.org>
2022-03-04 13:46:26 +0100

    Co-authored-by: Bohuslav <Bohooslav@users.noreply.github.com>

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mkdir letsencrypt
+mkdir /letsencrypt
 docker network create web
-docker-compose up -d --build --force-recreate --remove-orphans
+docker-compose up -d --build --force-recreate
 
 # Run migrations
 docker-compose exec web python manage.py migrate --noinput
@@ -11,6 +11,8 @@ docker-compose exec web python manage.py migrate --noinput
 docker-compose exec web python manage.py collectstatic --no-input --clear
 
 # Copy & activate unaccent rules
+# Here I use container name `postgres`. That feature is not important in testing. Maybe...
+# ill take your word for it -A
 docker cp ./django/sql/unaccent_plus.rules  postgres:/usr/local/share/postgresql/tsearch_data/unaccent_plus.rules
 docker exec -it postgres psql -U django -d bain -c "ALTER TEXT SEARCH DICTIONARY unaccent (RULES='unaccent_plus')"
 
