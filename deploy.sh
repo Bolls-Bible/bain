228a152b44624e7ed9c6089c0ea3c87a79f4412a
Bohuslav <bpavlisinec@gmail.com>
2022-03-07 19:30:50 +0200

    test docker commant

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ docker-compose exec web python manage.py collectstatic --no-input --clear
 # Here I use container name `postgres`. That feature is not important in testing. Maybe...
 # ill take your word for it -A
 docker cp ./django/sql/unaccent_plus.rules  postgres:/usr/local/share/postgresql/tsearch_data/unaccent_plus.rules
-docker exec -it postgres psql -U django -d bain -c "ALTER TEXT SEARCH DICTIONARY unaccent (RULES='unaccent_plus')"
+docker exec -i postgres psql -U django -d bain -c "ALTER TEXT SEARCH DICTIONARY unaccent (RULES='unaccent_plus')"
 
 echo "Done."
